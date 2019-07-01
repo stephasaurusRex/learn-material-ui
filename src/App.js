a696409c8782f3421f465a54d46bdb0bfd39063e
Stephanie Pitts <spitts@biztera.com>
2019-07-01 13:04:42 -0600

    fixing material-ui import

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <Typography>
       hello
-    </div>
+    </Typography>
   );
 }
 
